e43ec6fb0347c75e4e3ba3c5126007b091b4323a
Leandro Arakaki <lean_ariel@hotmail.com>
2022-05-25 19:42:51 -0300

    subiendo archivos

---
+++ src/App.js
@@ -0,0 +1,7 @@
+
+function App() {
+  return <div>Aguante la camada 32105!!!!
+  </div>;
+}
+
+export default App;
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,5 @@
+function App() {
+    return (<h1>Este es el componente NavBar</h1>);
+  }
+  
+  export default App;

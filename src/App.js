91ee0e9d3e1fa4e0ca4de47634f9835c5983f3e4
Leandro Arakaki <lean_ariel@hotmail.com>
2022-05-31 01:14:46 -0300

    subiendo archivos

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
+import './App.css';
+import MUNavBar from './components/MUNavBar';
+import MyComponent from './components/MyComponent/MyComponent';
+import NavBar from "./components/NavBar";
+import RBsNavBar from "./components/RBsNavBar";
 
 function App() {
-  return <div>Aguante la camada 32105!!!!
-  </div>;
+  return (
+    <>
+    
+    <MUNavBar />
+    <MyComponent />
+    
+  </>
+  );
 }
 
 export default App;
---
+++ src/components/MyComponent/MyComponent.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function MyComponent() {
+  return (
+    <div className='mydiv'>
+         
+        </div>
+  )
+}
+
+export default MyComponent
